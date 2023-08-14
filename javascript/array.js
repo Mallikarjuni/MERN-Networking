a6425f1a5b754304be7516617680f8c77e9a9a35
Mallikarjuni <mallikarjuni.ee19@rvce.edu.in>
2023-08-14 18:16:01 +0530

    initial commit

---
+++ javascript/array.js
@@ -0,0 +1,15 @@
+
+//literal
+var marks=[34,56,78,90,45];
+console.log(marks);
+
+//new key word
+var marks2=new Array();
+marks2[0]=67;
+marks2[1]=90;
+marks2[4]=50;
+console.log(marks2);
+
+//array constructor
+var marks3=new Array(84,92,78,60);
+console.log(marks3);
---
+++ javascript/myscript.js
@@ -0,0 +1,9 @@
+var a=1
+var b=2
+
+console.log(a+b);
+
+for(let i=1;i<=10;i++)
+{
+    console.log("hello"+i);
+}
